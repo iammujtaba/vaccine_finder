7b98388a9554602ae62ebaf7d375a77cf208a165
Mohd Mujtaba <mujtaba.aamir1@gmail.com>
2021-05-07 12:34:57 +0530

    fixed cors header issue and added requirements.txt

--- vaccine_by_pincode.py
+++ vaccine_by_pincode.py
@@ -7,7 +7,8 @@ def find_vaccine(pin_code,time_gap=None):
     next_slot = datetime.now()
     default_date_gap = time_gap or f"{next_slot.day}-{next_slot.month}-{next_slot.year}"
     url = f"{base_url}?pincode={pin_code}&date={default_date_gap}"
-    resp = requests.get(url).json()
+    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
+    resp = requests.get(url,headers=headers).json()
     centers = resp.get('centers')
     print("*"*50)
     if not centers:
